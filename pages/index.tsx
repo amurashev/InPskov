43707a029cd1d21d8e194f39aa122e75f40c0188
Andrey Murashev <andreymurashev@gmail.com>
2022-01-03 19:16:09 +0300

    Update pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,12 @@ const Home = () => {
         <div className={styles.brand}>
           <span className={styles.in}>In</span>Pskov
         </div>
-        <div className={styles.caption}>Спасибо, что были с нами</div>
+        <div className={styles.captionBox}>
+
+        <div className={styles.caption}>Сайт морально устарел.</div>
+        <div className={styles.caption}>Спасибо, что были с нами.</div>
+        <div className={styles.caption1}>Есть идеи? Пишите: <b>inpskovru@yandex.ru</b></div>
+        </div>
       </div>
     </Layout>
   );
