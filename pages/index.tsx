c80c0982e2cd1be7739ad800d67b55d34b3bf592
Andrey Murashev <andreymurashev@gmail.com>
2022-01-07 18:13:04 +0300

    New Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
 
 import styles from "../styles/Home.module.css";
@@ -6,11 +7,34 @@ const Home = () => {
   return (
     <Layout>
       <div className={styles.box}>
-        <div className={styles.brand}>
-          <span className={styles.in}>In</span>Pskov
-        </div>
-        <div className={styles.captionBox}>
-          <div className={styles.caption}>Поехали!</div>
+        <Image src="/images/krom.jpg" layout="fill" objectFit="cover" />
+        <div className={styles.fader}>
+          <div className={styles.content}>
+            <h1 className={styles.h1}>Псков</h1>
+            <p className={styles.description}>
+              Город в России, административный центр Псковской области, в
+              Северо-Западном федеральном округе. Является городом областного
+              подчинения; образует самостоятельное муниципальное образование
+              город Псков в статусе городского округа. Расположен на реке
+              Великой при слиянии её с рекой Псковой. Население - 209426 чел.
+            </p>
+            <ul  className={styles.list}>
+              <li className={styles.item}>
+                <span className={styles.itemLabel}>Погода</span>
+                <span className={styles.itemValue}>+5</span>
+              </li>
+
+              <li className={styles.item}>
+                <span className={styles.itemLabel}>Год Основания</span>
+                <span className={styles.itemValue}>903</span>
+              </li>
+
+              <li className={styles.item}>
+                <span className={styles.itemLabel}>Население</span>
+                <span className={styles.itemValue}>200 000</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </Layout>
